f7a53fce15ef8d89ed246bbc6bacc967356a2384
John-Olav Storvold <johnolos@stud.ntnu.no>
2014-02-17 14:03:43 +0100

    Removed unused imports

--- src/bomberman/connection/ClientPeer.java
+++ src/bomberman/connection/ClientPeer.java
@@ -1,11 +1,8 @@
 package bomberman.connection;
 
 import java.io.IOException;
-import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-
-import bomberman.connection.Server.ClientConnection;
 
 public class ClientPeer extends Thread {
 	
