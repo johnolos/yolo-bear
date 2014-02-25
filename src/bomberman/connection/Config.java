273fb164a1713cca0f60dbac2bfdec45c158e040
Brage Jahren <brage.jahren@gmail.com>
2014-02-25 14:05:20 +0100

    Ingen endringer

--- src/bomberman/connection/Config.java
+++ src/bomberman/connection/Config.java
@@ -7,6 +7,8 @@ public class Config {
 	
 	/* There should be changed for your devices */
 	public static final String SERVERIP = "78.91.12.244";
-	public static final String ANDROIDIP = "78.91.80.79";
+	public static final String ANDROIDIP = "78.91.1.195";
+	
+	
 	
 }
