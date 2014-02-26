2b508c3054e594b9a13ce2fa93e7ecf7903227f6
Johannes Lier <johannes.o.lier@gmail.com>
2014-02-26 11:32:52 +0100

    Bomb early implemented

--- src/bomberman/game/Bomb.java
+++ src/bomberman/game/Bomb.java
@@ -1,7 +1,36 @@
 package bomberman.game;
 
-public class Bomb {
-	
-//	Not yet implemented :P
+import sheep.game.Sprite;
+import sheep.graphics.Image;
+import android.graphics.Canvas;
 
+public class Bomb extends Sprite{
+	
+	private int blastRadius; //the "length" of the explosion
+	private Image bomb;
+	
+	public Bomb(){
+		if(Constants.screenHeight == 1600){
+			this.bomb = new Image(R.drawable.bomblarge);
+			this.setShape(120,120);
+		}
+		else if(Constants.screenHeight == 752){
+			this.bomb = new Image(R.drawable.bomblarge);
+			this.setShape(60,60);
+		}
+		else{
+			this.bomb = new Image(R.drawable.bomb);
+			this.setShape(40,40);
+		}
+		this.setView(bomb);
+	}
+	
+	public void update(float dt){
+		super.update(dt);
+	}
+	
+	public void draw(Canvas canvas){
+		super.draw(canvas);
+	}
+	
 }
