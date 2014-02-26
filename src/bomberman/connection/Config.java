faf1aa220ddcb341c263e1d2759eb3abac1d8d9c
Brage Jahren <brage.jahren@gmail.com>
2014-02-26 13:36:43 +0100

    big playergraphics for johnO

--- src/bomberman/connection/Config.java
+++ src/bomberman/connection/Config.java
@@ -6,8 +6,8 @@ public class Config {
 	public static final int ANDROIDPORT = 4093;
 	
 	/* There should be changed for your devices */
-	public static final String SERVERIP = "78.91.39.100";
+	public static final String SERVERIP = "78.91.14.219";
 
-	public static final String ANDROIDIP = "78.91.80.242";
+	public static final String ANDROIDIP = "78.91.0.183";
 
 }
