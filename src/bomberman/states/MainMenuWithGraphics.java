85915a9bf6ec2911a55170bc6ee1b77f3f19af0b
magnumog <magnumog@stud.ntnu.no>
2014-03-20 11:24:16 +0100

    that shit i tried earlier did not work

--- src/bomberman/states/MainMenuWithGraphics.java
+++ src/bomberman/states/MainMenuWithGraphics.java
@@ -1,55 +1,5 @@
 package bomberman.states;
 
-import android.graphics.Canvas;
-import android.graphics.Rect;
-import bomberman.game.Constants;
-import bomberman.game.R;
-import sheep.game.Sprite;
-import sheep.graphics.Image;
-import sheep.gui.TextButton;
-import sheep.gui.WidgetAction;
-import sheep.gui.WidgetListener;
-import sheep.math.BoundingBox;
+public class MainMenuWithGraphics {
 
-public class MainMenuWithGraphics extends Sprite {
-	private Image singlePlayer; 
-	private Image multiPlayer;
-	private BoundingBox box;
-	
-	/**
-	 * The constructor 
-	 */
-	public MainMenuWithGraphics(String buttonID, int x, int y) {
-		Rect bounds = null;
-		if(buttonID.equals("Single")) {
-			singlePlayer = new Image(R.drawable.singleplayer);
-			this.setView(singlePlayer);
-			this.setShape(100, 100); //DENNE Må ENDRES
-			this.setPosition(x, y);
-			float offsetX = 100*Constants.getReceivingXRatio();
-			float offsetY = 100*Constants.getReceivingYRatio();
-			bounds = new Rect((int)(x-offsetX),(int)(y-offsetY),(int)(x+offsetX),(int)(y+offsetY));
-		} else if(buttonID.equals("Multi")) {
-			multiPlayer = new Image(R.drawable.multiplayer);
-			this.setShape(100, 100); //DENNE Må ENDRES
-			this.setPosition(x, y);
-			float offsetX = 100*Constants.getReceivingXRatio();
-			float offsetY = 100*Constants.getReceivingYRatio();
-			bounds = new Rect((int)(x-offsetX),(int)(y-offsetY),(int)(x+offsetX),(int)(y+offsetY));
-		}
-		this.box = new BoundingBox(bounds);
-	}
-	
-	public void draw(Canvas canvas) {
-		super.draw(canvas);
-		
-	}
-	
-	public void update(float dt) {
-		super.update(dt);
-	}
-	
-	public BoundingBox getBounds() {
-		return this.box;
-	}
 }
