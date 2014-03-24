d63fef3a4ccda074611c93f67cbbf67e21d5ec8d
Brage Jahren <brage.jahren@gmail.com>
2014-03-24 11:16:59 +0100

    fixed tutorial

--- src/bomberman/graphics/TutorialImages.java
+++ src/bomberman/graphics/TutorialImages.java
@@ -12,21 +12,29 @@ import sheep.game.Sprite;
 import sheep.graphics.Image;
 
 public class TutorialImages extends Sprite {
-	private Image tutImgOne, tutImgTwo;
+	private Image tutImgOne, tutImgTwo, tutImgThree, tutImgFour, tutImgFive, tutImgSix;
 	private ArrayList<Image> img;
 	
 	public TutorialImages(float x, float y) {
 		img = new ArrayList<Image>();
-		tutImgOne = new Image(R.drawable.tutorialimgone);
-		this.setPosition(x, y);
-		this.setView(tutImgOne);
-		tutImgTwo = new Image(R.drawable.tutorialimgtwo);
+		tutImgOne = new Image(R.drawable.tutorialone);
+		tutImgTwo = new Image(R.drawable.tutorialtwo);
+		tutImgThree = new Image(R.drawable.tutorialthree);
+		tutImgFour = new Image(R.drawable.tutorialfour);
+		tutImgFive = new Image(R.drawable.tutorialfive);
+		tutImgSix = new Image(R.drawable.tutorialsix);
 		addToArray();
+		this.setPosition(x, y);
+		this.setView(img.get(0));
 	}
 	
 	private void addToArray() {
 		img.add(tutImgOne);
 		img.add(tutImgTwo);
+		img.add(tutImgThree);
+		img.add(tutImgFour);
+		img.add(tutImgFive);
+		img.add(tutImgSix);
 	}
 
 	public void update(float dt) {
@@ -38,7 +46,11 @@ public class TutorialImages extends Sprite {
 	}
 	
 	public void getViewFromArray(int n) {
-		this.setView(img.get(n));
+			this.setView(img.get(n));
+	}
+	
+	public int getArraySize() {
+		return this.img.size();
 	}
 
 }
