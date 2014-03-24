fc0d5344d5ba6f695c63b2a1b26ace1f9e3c3e28
magnumog <magnumog@stud.ntnu.no>
2014-03-24 22:14:33 +0100

    Finished javadoc for the classes i made, should we list the @atuhors in the different classes

--- src/bomberman/graphics/MainMenuBackground.java
+++ src/bomberman/graphics/MainMenuBackground.java
@@ -1,5 +1,7 @@
 package bomberman.graphics;
-
+/**
+ * This class extends Sprite
+ */
 import android.graphics.Canvas;
 import bomberman.game.Constants;
 import bomberman.game.R;
@@ -9,16 +11,27 @@ import sheep.graphics.Image;
 public class MainMenuBackground extends Sprite {
 	private Image startImage;
 	
+	/**
+	 * The constructor for this class, it loads an image sets it into the view and its position
+	 */
 	public MainMenuBackground() {
 		startImage = new Image(R.drawable.mainscreen);
 		this.setView(startImage);
 		this.setPosition((Constants.getScreenWidth()/2)-(startImage.getWidth()/2), (Constants.getScreenHeight()/2)-(startImage.getHeight()/2));
 	}
 	
+	/**
+	 * Draw function which draws the things onto the canvas, and draws the updated images onto the canvas.
+	 * @param Canvas canvas which you draw on.
+	 */
 	public void draw(Canvas canvas) {
 		super.draw(canvas);
 	}
 	
+	/**
+	 * This updates the view, if there is any changes in the view of the different buttons this updates function it.
+	 * @param float dt
+	 */
 	public void update(float dt) {
 		super.update(dt);
 	}
